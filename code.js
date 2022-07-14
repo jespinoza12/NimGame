9e9da62457b7f8788ef5cd457384ea1edd145645
jsingvah <79827219+jsingvah@users.noreply.github.com>
2022-07-14 00:24:31 -0600

    More CSS + Username Validation

--- code.js
+++ code.js
@@ -1,34 +1,48 @@
 var totalChips = 1
 var playerChips = 0
 //1 = Easy, 2 = Medium, 3 = Hard
-var Difficulty = 1
+var difficulty = 1
 var playerNum = 1;
 var cpu = false;
 
 var username = sessionStorage.getItem("username");
 var winner = sessionStorage.getItem("winner");
-var winnerName = sessionStorage.getItem("winnername")
-document.getElementById('player').innerHTML = username + "'s Turn"
 
+document.getElementById("player").innerHTML = username + "'s Turn"
 
+// Checks if player name is empty when name is submitted
 function playerName() {
-    username = document.getElementById("username").value;
-    document.getElementById("welcome").innerHTML = "Welcome " + username;
-    sessionStorage.setItem("username", username);
+    const player1 = document.getElementById("player1").value;
+    if (!player1) {
+        alert("Please enter a name.")
+    } else {
+        document.getElementById("greeting").innerHTML = "Welcome " + player1;
+        sessionStorage.setItem("username", player1);
+    }
+}
 
+// Checks if player name is empty when "Start Game" button is clicked
+// If empty, redirect back to home
+function checkName() {
+    const username = sessionStorage.getItem("username");
+    if (!username) {
+        alert("Please enter a name.")
+        const start = document.getElementById("start");
+        start.href = "home.html";
+    }
 }
 
 function chipCount(id) {
     if (playerChips > 2) {
-        alert("You Cannot select more than 3")
-        document.getElementById('inc').value = 3;
+        alert("You cannot select more than three chips.")
+        document.getElementById('counter').value = 3;
     } else {
-        document.getElementById('inc').value = ++playerChips;
-        document.getElementById(id).style.visibility="hidden";
+        document.getElementById('counter').value = ++playerChips;
+        document.getElementById(id).style.visibility = "hidden";
         totalChips++
     }
 
-    if (totalChips == 13){
+    if (totalChips == 13) {
         endGame()
     }
 }
@@ -38,48 +52,54 @@ function chipCount(id) {
 // }
 
 // function cpuMove(){
-//     if (Difficulty == 1) {
+//     if (difficulty == 1) {
 //         //Removes one of the visible chips
 
-//     }else if (Difficulty == 2){
+//     } else if (difficulty == 2) {
 //         //Removes random 1 or 2 chips from visible chips
 
-//     }else if (Difficulty == 3){
+//     } else if (difficulty == 3) {
 //         //Removes random 1 to 3 from visible chips
 //     }
 // }
 
-function endGame(){
-    if (winner == 1) {
-        sessionStorage.setItem("winnername", username + " has won the game!!");
-        location.href = '../NimGame/results.html';
-    }else{
-        sessionStorage.setItem("winnername",  "Player 2 has won the game!!");
-        location.href = '../NimGame/results.html';
+function endGame() {
+    location.href = "results.html";
+    console.log(winner);
+    if (winner == 1)
+    {
+        document.getElementById("result").innerHTML = username + " Wins!";
+    } else if (winner == 2) {
+        document.getElementById("result").innerHTML = "Player 2 Wins!"
     }
-
-    document.getElementById("1").innerHTML = winnerName;
-
 }
 
 function endTurn() {
-        if (cpu == false && playerNum == 1) {
-            playerChips = 0;
-            playerNum++
-            sessionStorage.setItem("winner", playerNum);
-            alert("It is now player 2's turn")
-            document.getElementById('player').innerHTML = "Player 2's Turn"
-        } else if (cpu == true && playerNum == 1){
-            playerChips = 0;
-            document.getElementById('player').innerHTML = "Cpu's Turn"
-            sessionStorage.setItem("winner", playerNum);
-            playerNum++
-            alert("It is now the Cpu's turn")
-        } else if (playerNum == 2){
-            playerChips = 0;
-            sessionStorage.setItem("winner", playerNum);
-            document.getElementById('player').innerHTML = username + "'s Turn"
-            playerNum--
-            alert("It is now " + username + "'s turn")
-        }
+    if (cpu == false && playerNum == 1) {
+        playerChips = 0;
+        playerNum++
+        sessionStorage.setItem("winner", playerNum);
+        alert("It is now player 2's turn")
+        document.getElementById('player').innerHTML = "Player 2's Turn"
+        document.getElementById('counter').value = 0;
+    // } else if (cpu == true && playerNum == 1) {
+    //     playerChips = 0;
+    //     document.getElementById('player').innerHTML = "Cpu's Turn"
+    //     sessionStorage.setItem("winner", playerNum);
+    //     playerNum++
+    //     alert("It is now the Cpu's turn")
+    //     document.getElementById('counter').value = 0;
+    } else if (playerNum == 2) {
+        playerChips = 0;
+        playerNum--
+        sessionStorage.setItem("winner", playerNum);
+        alert("It is now " + username + "'s turn")
+        document.getElementById('player').innerHTML = username + "'s Turn"
+        document.getElementById('counter').value = 0;
+    }
+}
+
+// If player plays again, the saved username will be set to empty
+function returnMenu() {
+    sessionStorage.setItem("username", "");
 }
